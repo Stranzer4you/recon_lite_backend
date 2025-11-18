3b03e55437b606477683986f943762a9ff8d5c90
satish abothula <satishabothula@satishs-MacBook-Air.local>
2025-11-18 21:12:39 +0530

    intial repo setup

---
+++ src/test/java/com/recon/lite/ReconLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recon.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
