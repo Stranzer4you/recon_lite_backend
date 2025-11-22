679e192e98cb0c47457dccb298e611c1ace82fc6
satish abothula <satishabothula@satishs-MacBook-Air.local>
2025-11-22 09:26:56 +0530

    default values for pagesize and pagenumber

--- src/main/java/com/recon/lite/model/request/GetAllTransactionsFilterDTO.java
+++ src/main/java/com/recon/lite/model/request/GetAllTransactionsFilterDTO.java
@@ -1,12 +1,14 @@
 package com.recon.lite.model.request;
 
 
+import com.recon.lite.utility.constants.ExceptionConstants;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class GetAllTransactionsFilterDTO {
     private String source;
     private String status;
-    private Integer pageNumber;
-    private Integer pageSize;
+    private Integer pageNumber=1;
+    private Integer pageSize=8;
 }
